2bd025d982323c79f5a4ebe8abf49934b21c9acb
Khun Tun(Jus Tin) <jajahuge890@gmail.com>
2022-09-10 18:06:56 +0630

    evening

---
+++ src/activity.js
@@ -0,0 +1,20 @@
+// START HEADER SECTION
+// Start nav bar
+const getNavbartoggler = document.querySelector(".navbar-toggler"),
+
+         getNavbartogglerIcon = document.querySelector(".navbar-toggler-icon");
+
+         getNavbartogglerIcon.addEventListener('click', (e) => {
+                getNavbartoggler.classList.toggle('change');
+         });
+
+// End nav bar
+// END HEADER SECTION
+
+
+// START FOOTER SECTION
+var getYears = document.querySelector("#get-years"),
+      getDate = new Date();
+getYears.textContent = getDate.getUTCFullYear();
+
+// END FOOTER SECTION
